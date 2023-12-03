525fa6a9946c5e26f8018b9cbad4d2d2ed3f6658
Raghav Singh <actualrag@gmail.com>
2023-12-04 00:19:09 +0530

    functions practice (commented code)

---
+++ functions.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class functions {
+
+//    public static void printMyName(String name){
+//        System.out.println(name);
+//        return;
+//    }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.next();
+//        printMyName("Your name is: " + name);
+//
+//    }
+//
+//    public static int calculateSum(int one, int two){
+//        int sum = one + two;
+//        return sum;
+//    }
+//
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        int one = sc.nextInt();
+//        int two = sc.nextInt();
+//
+//        int sum = calculateSum(one, two);
+//        System.out.println("Sum of your numbers is: "+ sum);
+//    }
+
+//    public static int myProduct(int a, int b) {
+//        int prod = a * b;
+//        return prod;
+//       // instead of defining these variables we can diretly write:
+//        return a * b;
+//        then call out our function at
+//        System.out.println(myProduct(int a, int b)) [Outside of this function at the main function]
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//
+//        int prod = myProduct(a, b);
+//        System.out.println("Product of your numbers is: "+ prod);
+//    }
+
+//    public static void myProduct(int facOne){
+//        if(facOne<0){
+//            System.out.println("Invalid Number!");
+//            return;
+//        }
+//        int factorial = 1;
+//        for (int i=facOne; i>=1; i--){
+//            factorial = factorial * i;
+//        }
+//        System.out.println("Your factorial is: "+ factorial);
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int facOne = sc.nextInt();
+//
+//        myProduct(facOne);
+//    }
+
+
+
+
+
+
+
+
+
+
+}
